d23b738b4cc1032748beb25c0205c0c5c05468e2
S Hafizh R Harsono <hafizhharsono@gmail.com>
2017-02-20 20:47:39 +0700

    Baca baik-baik soalnya

---
+++ bigrandom/answer.py
@@ -0,0 +1,16 @@
+class BigRandom:
+    def __init__(self):
+        self.data = "data.txt"
+        # add attributes if you need more
+
+    def answer(self):
+        noh = 0 # variable to store number of hashtag
+                # ommiting line number's hashtag
+        suc = 0 # variable to store sum of character's code in ascii,
+                # ommiting line number and its hashtag
+
+        # your algorithm
+
+        return (noh,suc)
+
+    # add methods if you need more
---
+++ caesar/answer.py
@@ -0,0 +1,13 @@
+class Caesar:
+    def __init__(self):
+        self.ciphertext = "ciphertext.txt"
+        # add attributes if you need more
+
+    def answer(self):
+        key = 0 # variable to store the key
+
+        # your algorithm
+
+        return (key)
+
+    # add methods if you need more
