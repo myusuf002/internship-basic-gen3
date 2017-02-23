47f00b3f0228f882aa745189b7720d0a258f2202
myusuf002 <abstraker@gmail.com>
2017-02-23 10:40:57 +0700

    Update answer.py

--- bigrandom/answer.py
+++ bigrandom/answer.py
@@ -8,9 +8,26 @@ class BigRandom:
                 # ommiting line number's hashtag
         suc = 0 # variable to store sum of character's code in ascii,
                 # ommiting line number and its hashtag
-
+        j = 0
         # your algorithm
+        data = open("data.txt", "r")
+        for i in range(10000):
+            line = data.readline()
+            j = 0
+            while line[j] != "#":
+                j +=1
+            j +=1
+            while j != len(line):
+                suc = suc + int(ord(line[j]))
+                if line[j] == "#":
+                    noh +=1
+                j +=1
 
+        print (noh)
+        print(suc)
         return (noh,suc)
 
     # add methods if you need more
+   
+ans = BigRandom()
+ans.answer()
